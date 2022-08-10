b80639b624aec2bc2e1b51513fb287be6853b77f
Johnexzy <obajohn75@gmail.com>
2022-08-10 06:31:41 +0100

    :construction: (added dom logics)

--- src/app.ts
+++ src/app.ts
@@ -77,5 +77,39 @@ const fetchNext = async (pageNumber: number) => {
     });
   }
 };
+const fetchPrev = async (pageNumber: number) => {
+  if (results && btn_prev.getAttribute("data-prevbtn") !== "0") {
+    table.removeChild(document.getElementsByTagName("tbody")[0]);
+    if (pageNumber == 1) btn_prev.disabled = true;
+    if (results![pageNumber]) {
+      traverseTable(pageNumber);
+      btn_next.setAttribute("data-nextbtn", `${pageNumber + 1}`);
+      btn_prev.setAttribute("data-prevbtn", `${pageNumber - 1}`);
+      return;
+    }
+    await fetchResource(pageNumber).then(() => {
+      if (results) {
+        traverseTable(pageNumber);
+        btn_next.setAttribute("data-nextbtn", `${pageNumber + 1}`);
+        btn_prev.setAttribute("data-prevbtn", `${pageNumber - 1}`);
+      }
+    });
+  }
+};
+const fetchResource = async (n: number): Promise<void> => {
+  img.style.display = "inline";
+  const req = await fetch(`${process.env.API_ENDPOINT}&page=${n}`);
+  const res: MainResponseType = await req.json();
+  img.style.display = "none";
+  results = res.results[0];
+};
+
+btn_next.addEventListener("click", () =>
+  fetchNext(parseInt(btn_next.getAttribute("data-nextbtn")!))
+);
+btn_prev.addEventListener("click", () =>
+  fetchPrev(parseInt(btn_prev.getAttribute("data-prevbtn")!))
+);
+document.addEventListener("DOMContentLoaded", startApp);
 
 document.addEventListener('DOMContentLoaded', startApp);
